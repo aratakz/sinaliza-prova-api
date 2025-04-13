8f3f78cead96d1cde4e8df3d47390fc3600aad37
Daniel Pessoa <danielpessoa432@gmail.com>
2025-04-13 17:02:09 -0300

    compare password

--- src/domain/Security.ts
+++ src/domain/Security.ts
@@ -2,6 +2,7 @@ import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import { UserRepository } from '../repository/UserRepository';
 import { AuthException } from './exception/AuthExceptoion';
+import * as bcrypt from 'bcrypt';
 
 export class Security {
     private userRepository: UserRepository;
@@ -12,9 +13,18 @@ export class Security {
 
     async getCredentials(userName: string, password: string) {
         if (process.env.TOKEN_SECRET) {
-            if (!await this.userRepository.findByUserName(userName)) {
+            const user = await this.userRepository.findByUserName(userName);
+            if (!user) {
                 throw new AuthException();
             }
+            if (!password) {
+                throw new AuthException();
+
+            }
+            if (!await bcrypt.compare(password, user.password)) {
+                throw new AuthException();
+            }
+            
             return jwt.sign({ id: '' }, process.env.TOKEN_SECRET, {
                 expiresIn: '2h'
             });
